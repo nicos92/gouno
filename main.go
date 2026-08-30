e66be62098035e289563565d414c8e7bac8f61c5
Nicolas sandoval <nicolassandoval140692@gmail.com>
2026-08-30 01:25:49 -0300

    Siguiente Commit

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "github.com/nicos92/gouno/variables"
+import (
+	"fmt"
+
+	"github.com/nicos92/gouno/variables"
+)
 
 func main() {
 
-	variables.MostrarEnteros()
+	// variables.MostrarEnteros()
+	// variables.RestoVariables()
+	estado, texto := variables.ConviertoATexto(1234)
+
+	fmt.Println(estado)
+	fmt.Println(texto)
+
 }
---
+++ variables/resto.go
@@ -0,0 +1,30 @@
+package variables
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+var Nombre string
+var Estado bool
+var Sueldo float32
+var Fecha time.Time
+
+func RestoVariables() {
+	// MostrarEnteros()
+
+	Nombre = "Pedro"
+	Estado = true
+	Sueldo = 1444.21
+	Fecha = time.Now()
+	fmt.Println(Nombre)
+	fmt.Println(Estado)
+	fmt.Println(Sueldo)
+	fmt.Println(Fecha)
+}
+
+func ConviertoATexto(numero int) (bool, string) {
+	texto := strconv.Itoa(numero)
+	return true, texto
+}
